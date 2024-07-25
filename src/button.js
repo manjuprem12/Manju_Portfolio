8ebe1177ce00f703d830184582b3d8553c812bfd
Manju Prem <50079772+manjuprem12@users.noreply.github.com>
2024-07-25 09:46:40 +0200

    Add files via upload

---
+++ src/button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './button.css';
+// import { AiOutlineArrowLeft } from 'react-icons/ai';
+
+export default ({ className, onClick, children }) => {
+    return (
+        <button
+            className={`style-button ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
